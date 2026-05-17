5d6116217ae91034aaf9d812e62efbb6bb015384
Ian Vinícius <ianvinicioss@gmail.com>
2026-05-17 00:28:32 -0300

    chore(app): tooling, dependencies and native font linking

--- tradeup-app/babel.config.js
+++ tradeup-app/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
   plugins: [
+    '@babel/plugin-transform-export-namespace-from',
     ['@babel/plugin-proposal-decorators', { version: '2023-11' }],
     ['module:react-native-dotenv'],
     [
--- tradeup-app/jest.config.js
+++ tradeup-app/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-css-interop|nativewind)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-native-vector-icons|react-native-css-interop|nativewind)/)',
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
